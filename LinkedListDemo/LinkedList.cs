dac68a303e89790f7c19ce29215062ccfc0aa8ca
[ganapat] <ganapatisakatwa@gmail.com>
2022-09-28 10:31:04 +0530

    [Ganapati]Linked List AddLast

---
+++ LinkedListDemo/LinkedList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListDemo
+{
+    public class LinkedList
+    {
+        public Node head;
+        public void AddLast(int data)
+        {
+            Node newnode = new Node(data);
+            if (head==null)
+            {
+                head = newnode;
+                Console.WriteLine("{0} new Node inserted into LinkedList", data);
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.next!=null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = newnode;
+            }
+            Console.WriteLine("{0} new Node inserted into LinkedList", newnode.data);
+        }
+
+        public void Display()
+        {
+            Node temp = head;
+            if(temp==null)
+            {
+                Console.WriteLine("LinkedList is Empty");
+            }
+            else
+            {
+                while(temp!=null)
+                {
+                    Console.WriteLine(temp.data);
+                    temp = temp.next;
+                }
+
+            }
+        }
+    }
+}
---
+++ LinkedListDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace LinkedListDemo
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Computation of Linked List");
+            LinkedList linkedlist = new LinkedList();
+            linkedlist.AddLast(56);
+            linkedlist.AddLast(30);
+            linkedlist.AddLast(70);
+            linkedlist.Display();
+        }
+    }
+}
